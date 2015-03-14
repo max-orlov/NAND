0fc9808684f7b70ce39129cfc261538336c40a95
max-orlov <maxim@gigaspaces.com>
2015-03-14 13:54:15 +0200

    Fill.asm
    first draft..

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,51 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+@i
+M=0
+@SWITCH
+M=0
+(LOOP)
+	@24576		//Listen to keyboard
+	D=M
+	@WHITEN
+	D; JEQ
+	@BLACKEN
+	D; JGT
+	(BLACKEN)		//Blacken the screen
+		@SWITCH		// Check for swith from white to black
+		D=M
+		M=1
+		@CLEAR_COUNT
+		D; JEQ
+		
+		@SCREEN		// Fill the screen with black dots
+		D=A
+		@i
+		A=D+M
+		M=1
+		@CONTINUE
+		0; JMP
+	(WHITEN)		//Whiten the screen
+		@SWITCH		// Check for a switch from black to white
+		D=M-1
+		M=0
+		@CLEAR_COUNT
+		D; JEQ
+		
+		@SCREEN		// Clear the screen
+		D=A
+		@i
+		A=D+M
+		M=0
+	(CONTINUE)
+		@i
+		M=M+1
+		@LOOP
+		0; JMP
+	(CLEAR_COUNT)
+		@i
+		M=0
+		@LOOP
+		0; JMP
+(END)
