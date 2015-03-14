ade206fd34d7e857f2a5cad5a1049ce5bc1a01ad
jonnydman <jonnydman@gmail.com>
2015-03-14 12:16:02 +0200

    added the MULT.
    the tests ran successfully.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	// we add R0 to R2 (starting from 0), R1 times
+	
+	@R2		// set R2 to 0.
+	M=0
+	
+	@i		// set i to 0
+	M=1
+(LOOP)
+	@i		// if (i - in2) >= 0 GOTO END
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGT
+	@R0	// R2 = R2 + in1
+	D=M
+	@R2
+	M=D+M
+	@i		// i++
+	M=M+1
+	@LOOP	// GOTO LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
