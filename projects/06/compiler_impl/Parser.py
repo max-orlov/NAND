e33bd7f06b083fe237cb3181f9c01be1b5fc8324
jonnydman <jonnydman@gmail.com>
2015-04-06 02:24:52 +0300

    added comments to Parser.

--- projects/06/compiler_impl/Parser.py
+++ projects/06/compiler_impl/Parser.py
@@ -7,23 +7,31 @@ class CommandType(Enum):
 
 # this class handles the parsing of a given file.
 class Parser:
+    # initializes the needed parameters.
     def __init__(self, filename):
-        self._fileLines = enumerate(open(filename).readlines())
-
+        self._file = open(filename)
+        self._fileLines = self._file.readlines()
+        self._fileLinesIter = enumerate(self._fileLines)
         self._currentCommand = ''
         self._currentLine = ''
-        self._hasCommands = False
 
+    # checks if there are more valid commands in the file (comments and empty lines are not valid).
     def hasMoreCommands(self):
-        index, line = self._fileLines.next()
-        while line.strip().startswith("#"):
-            index, line = str(self._fileLines.next()).strip()
+        index, line = self._fileLinesIter.next()
+        while line.strip().startswith("//") or len(line.strip()) == 0:
+            index, line = str(self._fileLinesIter.next()).strip()
+            if index == len(self._fileLines):   # reached the end..
+                self._file.close()
+                return False
+
         self._currentLine = line
-        return index < len(self._fileLines)
+        return True
 
+    # sets the current command to the current valid line (without comments).
     def advance(self):
-        self._currentCommand = self._currentLine[:self._currentLine.find("#")].strip()
+        self._currentCommand = self._currentLine[:self._currentLine.find("//")].strip()
 
+    # returns the type of the current command from the CommandType enum.
     def commandType(self):
         if "=" in self._currentCommand or ";" in self.currentLine:
             return CommandType.C_COMMAND
@@ -32,18 +40,34 @@ class Parser:
         else:
             return CommandType.A_COMMAND
 
+    ###
+    #  returns symbol or decimal number from command.
+    #  Note: assumed that the command is L_COMMAND or A_COMMAND.
+    ###
     def symbol(self):
         if self._currentCommand.startswith("@"):
             return self._currentCommand[1:]
         return self._currentCommand[1: self._currentCommand.find(")")]
 
+    ###
+    #  returns 'dest' part (mnemonic) from command.
+    #  Note: assumed that the command is L_COMMAND or A_COMMAND.
+    ###
     def dest(self):
         return self._currentCommand[: self._currentLine.find("=")].strip()
 
+    ###
+    #  returns 'comp' part (mnemonic) from command.
+    #  Note: assumed that the command is L_COMMAND or A_COMMAND.
+    ###
     def comp(self):
         if "=" in self._currentCommand:
             return self._currentCommand[self._currentLine.find("=") + 1: self._currentLine.find(";")].strip()
         return self._currentCommand[: self._currentLine.find(";")].strip()
 
+    ###
+    #  returns 'jump' part (mnemonic) from command.
+    #  Note: assumed that the command is L_COMMAND or A_COMMAND.
+    ###
     def jump(self):
         return self._currentCommand[self._currentLine.find(";") + 1:].strip()
