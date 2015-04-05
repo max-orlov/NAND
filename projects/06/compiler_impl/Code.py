3d7ba2d17445d933f8628cf1916e21c7e05bae2f
jonnydman <jonnydman@gmail.com>
2015-04-06 02:25:31 +0300

    added skeleton for Code.

---
+++ projects/06/compiler_impl/Code.py
@@ -0,0 +1,14 @@
+# this class handles parsing HACK mnemonics into C-instructions binary codes.
+
+class Code:
+    def __init__(self):
+        pass
+
+    def dest(self, mnemonic):
+        pass
+
+    def comp(self, mnemonic):
+        pass
+
+    def jump(self, mnemonic):
+        pass
