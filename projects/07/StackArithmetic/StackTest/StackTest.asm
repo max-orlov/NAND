72dce77ea7dd9d2547ef3aaf78095c72f4672756
jonnydman <jonnydman@gmail.com>
2015-05-02 19:55:05 +0300

    adding all .asm files which we generated.

---
+++ projects/07/StackArithmetic/StackTest/StackTest.asm
@@ -0,0 +1,736 @@
+@17
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_2_jeq_1lt
+D; JLT
+@15
+D=M
+@_StackTest_2_jeq_fin
+D; JGE
+D=1
+@_StackTest_2_jeq_check
+0; JMP
+(_StackTest_2_jeq_1lt)
+@15
+D=M
+@_StackTest_2_jeq_fin
+D; JLT
+D=-1
+@_StackTest_2_jeq_check
+0; JMP
+(_StackTest_2_jeq_fin)
+@13
+D=M
+(_StackTest_2_jeq_check)
+@_StackTest_2_jeq_is
+D; JEQ
+@_StackTest_2_jeq_not
+0; JMP
+(_StackTest_2_jeq_is)
+D=-1
+@_StackTest_2_jeq_end
+0; JMP
+(_StackTest_2_jeq_not)
+D=0
+(_StackTest_2_jeq_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@16
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_5_jeq_1lt
+D; JLT
+@15
+D=M
+@_StackTest_5_jeq_fin
+D; JGE
+D=1
+@_StackTest_5_jeq_check
+0; JMP
+(_StackTest_5_jeq_1lt)
+@15
+D=M
+@_StackTest_5_jeq_fin
+D; JLT
+D=-1
+@_StackTest_5_jeq_check
+0; JMP
+(_StackTest_5_jeq_fin)
+@13
+D=M
+(_StackTest_5_jeq_check)
+@_StackTest_5_jeq_is
+D; JEQ
+@_StackTest_5_jeq_not
+0; JMP
+(_StackTest_5_jeq_is)
+D=-1
+@_StackTest_5_jeq_end
+0; JMP
+(_StackTest_5_jeq_not)
+D=0
+(_StackTest_5_jeq_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@16
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_8_jeq_1lt
+D; JLT
+@15
+D=M
+@_StackTest_8_jeq_fin
+D; JGE
+D=1
+@_StackTest_8_jeq_check
+0; JMP
+(_StackTest_8_jeq_1lt)
+@15
+D=M
+@_StackTest_8_jeq_fin
+D; JLT
+D=-1
+@_StackTest_8_jeq_check
+0; JMP
+(_StackTest_8_jeq_fin)
+@13
+D=M
+(_StackTest_8_jeq_check)
+@_StackTest_8_jeq_is
+D; JEQ
+@_StackTest_8_jeq_not
+0; JMP
+(_StackTest_8_jeq_is)
+D=-1
+@_StackTest_8_jeq_end
+0; JMP
+(_StackTest_8_jeq_not)
+D=0
+(_StackTest_8_jeq_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@892
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_11_jlt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_11_jlt_fin
+D; JGE
+D=1
+@_StackTest_11_jlt_check
+0; JMP
+(_StackTest_11_jlt_1lt)
+@15
+D=M
+@_StackTest_11_jlt_fin
+D; JLT
+D=-1
+@_StackTest_11_jlt_check
+0; JMP
+(_StackTest_11_jlt_fin)
+@13
+D=M
+(_StackTest_11_jlt_check)
+@_StackTest_11_jlt_is
+D; JLT
+@_StackTest_11_jlt_not
+0; JMP
+(_StackTest_11_jlt_is)
+D=-1
+@_StackTest_11_jlt_end
+0; JMP
+(_StackTest_11_jlt_not)
+D=0
+(_StackTest_11_jlt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@892
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_14_jlt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_14_jlt_fin
+D; JGE
+D=1
+@_StackTest_14_jlt_check
+0; JMP
+(_StackTest_14_jlt_1lt)
+@15
+D=M
+@_StackTest_14_jlt_fin
+D; JLT
+D=-1
+@_StackTest_14_jlt_check
+0; JMP
+(_StackTest_14_jlt_fin)
+@13
+D=M
+(_StackTest_14_jlt_check)
+@_StackTest_14_jlt_is
+D; JLT
+@_StackTest_14_jlt_not
+0; JMP
+(_StackTest_14_jlt_is)
+D=-1
+@_StackTest_14_jlt_end
+0; JMP
+(_StackTest_14_jlt_not)
+D=0
+(_StackTest_14_jlt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_17_jlt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_17_jlt_fin
+D; JGE
+D=1
+@_StackTest_17_jlt_check
+0; JMP
+(_StackTest_17_jlt_1lt)
+@15
+D=M
+@_StackTest_17_jlt_fin
+D; JLT
+D=-1
+@_StackTest_17_jlt_check
+0; JMP
+(_StackTest_17_jlt_fin)
+@13
+D=M
+(_StackTest_17_jlt_check)
+@_StackTest_17_jlt_is
+D; JLT
+@_StackTest_17_jlt_not
+0; JMP
+(_StackTest_17_jlt_is)
+D=-1
+@_StackTest_17_jlt_end
+0; JMP
+(_StackTest_17_jlt_not)
+D=0
+(_StackTest_17_jlt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32767
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_20_jgt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_20_jgt_fin
+D; JGE
+D=1
+@_StackTest_20_jgt_check
+0; JMP
+(_StackTest_20_jgt_1lt)
+@15
+D=M
+@_StackTest_20_jgt_fin
+D; JLT
+D=-1
+@_StackTest_20_jgt_check
+0; JMP
+(_StackTest_20_jgt_fin)
+@13
+D=M
+(_StackTest_20_jgt_check)
+@_StackTest_20_jgt_is
+D; JGT
+@_StackTest_20_jgt_not
+0; JMP
+(_StackTest_20_jgt_is)
+D=-1
+@_StackTest_20_jgt_end
+0; JMP
+(_StackTest_20_jgt_not)
+D=0
+(_StackTest_20_jgt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32767
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_23_jgt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_23_jgt_fin
+D; JGE
+D=1
+@_StackTest_23_jgt_check
+0; JMP
+(_StackTest_23_jgt_1lt)
+@15
+D=M
+@_StackTest_23_jgt_fin
+D; JLT
+D=-1
+@_StackTest_23_jgt_check
+0; JMP
+(_StackTest_23_jgt_fin)
+@13
+D=M
+(_StackTest_23_jgt_check)
+@_StackTest_23_jgt_is
+D; JGT
+@_StackTest_23_jgt_not
+0; JMP
+(_StackTest_23_jgt_is)
+D=-1
+@_StackTest_23_jgt_end
+0; JMP
+(_StackTest_23_jgt_not)
+D=0
+(_StackTest_23_jgt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@15
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@14
+M=D
+@15
+D=D-M
+@13
+M=D
+@14
+D=M
+@_StackTest_26_jgt_1lt
+D; JLT
+@15
+D=M
+@_StackTest_26_jgt_fin
+D; JGE
+D=1
+@_StackTest_26_jgt_check
+0; JMP
+(_StackTest_26_jgt_1lt)
+@15
+D=M
+@_StackTest_26_jgt_fin
+D; JLT
+D=-1
+@_StackTest_26_jgt_check
+0; JMP
+(_StackTest_26_jgt_fin)
+@13
+D=M
+(_StackTest_26_jgt_check)
+@_StackTest_26_jgt_is
+D; JGT
+@_StackTest_26_jgt_not
+0; JMP
+(_StackTest_26_jgt_is)
+D=-1
+@_StackTest_26_jgt_end
+0; JMP
+(_StackTest_26_jgt_not)
+D=0
+(_StackTest_26_jgt_end)
+
+@SP
+A=M
+M=D
+@0
+M=M+1
+@57
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@31
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@53
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M+D
+@SP
+A=M
+M=D
+@0
+M=M+1
+@112
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+D=-D
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M&D
+@SP
+A=M
+M=D
+@0
+M=M+1
+@82
+D=A
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M|D
+@SP
+A=M
+M=D
+@0
+M=M+1
+@SP
+M=M-1
+A=M
+D=!M
+@SP
+A=M
+M=D
+@0
+M=M+1
