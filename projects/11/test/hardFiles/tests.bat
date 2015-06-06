53ad8de88b88d4741fa49f2b9c0d42639b76ce5b
jonnydman <jonnydman@gmail.com>
2015-06-06 20:11:50 +0300

    all tests are passing (not including try2 which isn't supposed to)

---
+++ projects/11/test/hardFiles/tests.bat
@@ -0,0 +1,12 @@
+echo '====FrogGame.vm====' 
+fc /w FrogGame.vm FrogGameA.vm
+echo '====GameControl.vm====' 
+fc /w GameControl.vm GameControlA.vm
+echo '====in3.vm====' 
+fc /w in3.vm in3A.vm
+echo '====simpleCommentTest.vm====' 
+fc /w simpleCommentTest.vm simpleCommentTestA.vm
+echo '====Surface.vm====' 
+fc /w Surface.vm SurfaceA.vm
+echo '====try2.vm===='
+fc /w try2.vm try2A.vm
